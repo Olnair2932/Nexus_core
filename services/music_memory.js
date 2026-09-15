f73853543091c0cf5f506801f894b5cab4ec5e86
Olnair2932 <olnairpereira@gmail.com>
2026-09-15 03:09:17 -0300

    Corrige acesso Firebase na memória musical

--- services/music_memory.js
+++ services/music_memory.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const path = require("path");
-const db = require("./firebase_admin");
+const { database } = require("./firebase_admin");
+const { ref, get } = require("firebase-admin/database");
 
 const FILE = path.join(
     __dirname,
@@ -256,7 +257,7 @@ async function procurarMemoria(texto, uid) {
         try {
 
             const snap =
-                await db.ref(`playlists/${uid}`).once("value");
+                await get(ref(database, `playlists/${uid}`));
 
             const playlist =
                 snap.val();
